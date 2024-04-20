b8b6cfb8850bb2a8273096162ce13d49e701465d
Danila Buyakevich <boss.buyakevich@mail.ru>
2024-04-20 22:22:18 +0300

    Creating a dynamic room

--- AlienLicense/Assets/App/Scripts/GameScene/GridManager.cs
+++ AlienLicense/Assets/App/Scripts/GameScene/GridManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace App.Scripts.GameScene
@@ -9,40 +10,63 @@ namespace App.Scripts.GameScene
     public class GridManager : MonoBehaviour
     {
         [SerializeField] private GameObject gridPrefab;
+        [SerializeField] private GameObject wallPrefab;
+        [SerializeField] private GameObject windowPrefab;
         [SerializeField] Vector2Int gridSize;
-        [SerializeField] private GameObject[] items;
+        [SerializeField] private GameObject[] exitVariants;
 
         private Vector3 _gridInitialPosition;
-
+        private float _wallOffsetY;
+        private float _windowOffsetY;
         public event Action<Vector2Int> OnGridGenerated;
     
         void Start()
         {
-            _gridInitialPosition = new Vector3(0.5f, gridPrefab.transform.position.y / 2, 0.5f);
             GenerateGrid();
         }
 
+        private void RoomParametersSetup()
+        {
+            _gridInitialPosition = new Vector3(0.5f, gridPrefab.transform.position.y, 0.5f);
+            _wallOffsetY = wallPrefab.transform.localScale.y / 2;
+            _windowOffsetY = windowPrefab.transform.localScale.y / 2;
+        }
         [Button]
         private void GenerateGrid()
         {
             ClearGrid();
-            for (int x = 0; x < gridSize.x; x++)
+            RoomParametersSetup();
+            for (int x = -1; x <= gridSize.x; x++)
             {
-                for (int z = 0; z < gridSize.y; z++)
+                for (int z = -1; z <= gridSize.y; z++)
                 {
-                    Vector3 gridPosition = new Vector3(_gridInitialPosition.x + x, _gridInitialPosition.y,
-                        _gridInitialPosition.z + z);
-
-                    GameObject gridObj = Instantiate(gridPrefab, gridPosition, Quaternion.identity);
-                    gridObj.transform.parent = transform;
-
-                    /*int randomIndex = Random.Range(0, items.Length);
-                    GameObject item = Instantiate(items[randomIndex], gridPosition, Quaternion.identity);
-                    item.transform.parent = gridObj.transform;*/
+                    if (x == -1 || z == -1 || x == gridSize.x || z == gridSize.y)
+                    {
+                        Vector3 wallPosition = new Vector3(_gridInitialPosition.x + x, _gridInitialPosition.y, _gridInitialPosition.z + z);
+                        GameObject wallObj;
+                        if ((x == gridSize.x - 1 && z == gridSize.y) /*|| (x == gridSize.x && z == gridSize.y - 1)*/)
+                        {
+                            windowPrefab = exitVariants[Random.Range(0, exitVariants.Length)];
+                            wallObj = Instantiate(windowPrefab, wallPosition + new Vector3(0,_windowOffsetY,0), Quaternion.identity); 
+                        }
+                        else
+                        {
+                            wallObj = Instantiate(wallPrefab, wallPosition + new Vector3(0,_wallOffsetY,0), Quaternion.identity);
+                        }
+                        wallObj.transform.parent = transform;
+                    }
+                    else
+                    {
+                        Vector3 gridPosition = new Vector3(_gridInitialPosition.x + x, _gridInitialPosition.y, _gridInitialPosition.z + z);
+                        GameObject gridObj = Instantiate(gridPrefab, gridPosition, Quaternion.identity);
+                        gridObj.transform.parent = transform;
+                    }
                 }
             }
             OnGridGenerated?.Invoke(gridSize);
         }
+
+        [Button]
         private void ClearGrid()
         {
             List<GameObject> children = new List<GameObject>();
